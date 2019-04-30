633e18f7cc776d563929ad45749e97d360eeb370
samuel day <samgrd.mail.fresnostate.edu>
2019-04-29 17:34:29 -0700

    pushing up to exam-end branch

--- src/app/test-score/test-score.component.ts
+++ src/app/test-score/test-score.component.ts
@@ -4,6 +4,14 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 
+export interface ITest {
+  id?: number;
+  testName: string;
+  pointsPossible: number;
+  pointsReceived: number;
+  percentage: number;
+  grade: string;
+}
 
 @Component({
   selector: 'app-test-score',
@@ -12,7 +20,7 @@ import { ToastService } from '../toast/toast.service';
 })
 export class TestScoreComponent implements OnInit {
 
-  tests: Array<any> = [];
+  tests: Array<ITest> = [];
   constructor(
     private http: Http,
     private activatedRoute: ActivatedRoute,
@@ -20,8 +28,124 @@ export class TestScoreComponent implements OnInit {
     private toastService: ToastService
   ) { }
 
-  async ngOnInit() {
+  name = '';
 
+  async ngOnInit() {
+    this.tests = await this.loadTests();
   }
+
+  async loadTests() {
+    let tests = JSON.parse(localStorage.getItem('tests'));
+    if (tests && tests.length > 0) {
+    } else {
+      tests = await this.loadTestsFromJson();
+    }
+    console.log('this.contacts from ngOninit... ', this.tests);
+    this.tests = tests;
+    return tests;
+  }
+
+  async loadTestsFromJson() {
+    const tests = await this.http.get('assets/tests.json').toPromise();
+    return tests.json();
+  }
+
+  addTest() {
+    const test: ITest = {
+      id: null,
+      testName: null,
+      pointsPossible: null,
+      pointsReceived: null,
+      percentage: null,
+      grade: null
+    };
+    this.tests.unshift(test);
+    this.saveToLocalStorage();
+  }
+
+  deleteTest(index: number) {
+    this.tests.splice(index, 1);
+    this.saveToLocalStorage();
+  }
+
+  saveTest() {
+    this.saveToLocalStorage();
+    this.toastService.showToast('success', 5000, 'Success: Items Saved!');
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('tests', JSON.stringify(this.tests));
+  }
+
+  computeGrade() {
+    const data = this.calculate();
+    localStorage.setItem('calculatedData', JSON.stringify(data));
+    this.router.navigate(['home', data]);
+  }
+
+  // calculateGrade() {
+  //   this.calculate();
+  //   switch (true) {
+  //     case totalPercentage >= 90:
+  //       grade = 'A';
+  //       break;
+  //     case score >= 80:
+  //       grade = 'B';
+  //       break;
+  //     case score >= 70:
+  //       grade = 'C';
+  //       break;
+  //     case score >= 60:
+  //       grade = 'D';
+  //       break;
+  //     default:
+  //       grade = 'F';
+  //       break;
+// }
+
+  // }
+
+  calculate() {
+    let pointsPossible = 0;
+    let pointsReceived = 0;
+    let percentage = 0;
+    // let grade = 0;
+    for (let i = 0; i < this.tests.length; i++) {
+      pointsPossible += this.tests[i].pointsPossible;
+      pointsReceived += this.tests[i].pointsReceived;
+      percentage += (pointsReceived / pointsPossible);
+      // grade = this.tests[i].
+    }
+    return {
+      totalPointsPossible: pointsPossible,
+      totalPointsReceived: pointsReceived,
+      totalPercentage: percentage / this.tests.length,
+      // finalGrade:
+      };
+    }
+
+    // Name() {
+    //   const commaIndex = this.name.indexOf(', ');
+    //   let error = false;
+
+    //   if (this.name === '') {
+    //     // this.errorMessage = 'Name must not be empty!';
+    //     this.toastService.showToast('warn', 5000, 'Name must not be null');
+    //     error = true;
+    //   } else if (commaIndex === -1) {
+    //     // this.errorMessage = 'Name must have a comma and a space!';
+    //     this.toastService.showToast('warn', 5000, 'Name must contain a comma and a space');
+    //     error = true;
+    //   }
+
+    //   if (!error) {
+    //     const firstName = this.name.slice(commaIndex + 1, this.name.length);
+    //     const lastName = this.name.slice(0, commaIndex);
+    //     const fullName = firstName + ' ' + lastName;
+    //     return{
+    //       fullName
+    //     };
+    //   }
+    // }
 
 }
